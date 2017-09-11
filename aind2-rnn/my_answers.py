4e1950c99ec15b2ce6cd3411cb7f06a91c1af4dc
Wei Chiet <weichiet@outlook.com>
2017-09-11 22:17:08 +0800

    Complete build_part1_RNN function

--- aind2-rnn/my_answers.py
+++ aind2-rnn/my_answers.py
@@ -28,7 +28,12 @@ def window_transform_series(series, window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
-    pass
+    
+    model = Sequential()
+    model.add(LSTM(units = 5, input_shape = (window_size, 1)))
+    model.add(Dense(units = 1))
+    
+    return model
 
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
